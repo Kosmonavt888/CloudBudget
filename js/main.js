5915ecfd84c3db58f0a2a812670eceaeda8c457d
Kosmonavt888 <asya.aleksandra@mail.ru>
2026-01-28 14:24:19 +0300

    final_version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+document.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances_sidenav = M.Sidenav.init(sidenav);
+    var modal = document.querySelectorAll('.modal');
+    var instances_modal = M.Modal.init(modal);
+    var gallery = document.querySelectorAll('.materialboxed');
+    var instances_gallery = M.Materialbox.init(gallery);
   });
